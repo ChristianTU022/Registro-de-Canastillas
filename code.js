7219089aedf5db380977c4e5cfa8d3d0c5ad91ea
christiantu022 <cratrianac@udistrital.edu.co>
2024-04-01 16:08:41 -0500

    Creacion de apartado Limpieza nivel 1 y parte del apartado de calidad, ademas de tocar algunos bugs y mejorar la responsividad, se elimina carpeta

--- code.js
+++ code.js
@@ -7,3 +7,13 @@ function getHtmlData (nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function conectionSheets() {
+    //Conectar Sheets a AppScript
+   const sheetId = '1M4nEb_9WDStyuydThrceuUHJE4nWfCJNz0Gp_AU7gXE';
+   const sheet = SpreadsheetApp.openById(sheetId);
+    //Conectar Hojas especificas
+   const p_Answer_Boxes = sheet.getSheetByName('Answer_Boxes');
+   
+   return { sheet, p_Answer_Boxes };
+ }
